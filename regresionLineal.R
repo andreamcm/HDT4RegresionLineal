45b411f254425ab8de4e3e3d6914a56d3b6cdd53
andreamcm <cor16076@uvg.edu.gt>
2019-03-15 10:00:50 -0600

    Avances

--- regresionLineal.R
+++ regresionLineal.R
@@ -31,26 +31,119 @@ str(datos)
 porcentaje <- 0.7 # Se define el porcentaje de los datos de entrenamiento
 set.seed(123) # Para que no se repita la seleccion de datos cada vez que se corra el programa
 
-datos$y <- datos$AdoptionSpeed
-corte <- sample(nrow(datos), nrow(datos)*porcentaje)
+datos$y <- datos$Type # Se definen los datos de comparacion. En este caso el tipo del animal.
+corte <- sample(nrow(datos), nrow(datos)*porcentaje) # Se realiza el corte de los datos
+
 train <- datos[corte, ] # Datos de entrenamiento
 test <- datos[-corte, ] # Datos de prueba
 
-fitLMPW <- lm(AdoptionSpeed~Age, data = train)
-prediccionRL <- predict(fitLMPW, newdata = test)
+# AdoptionSpeed y Age
+fitLMPW1 <- lm(AdoptionSpeed~Age, data = train)
+prediccionRL1 <- predict(fitLMPW1, newdata = test)
 
-resultados <- data.frame(test$AdoptionSpeed, prediccionRL)
-resultados$variacion <- abs(resultados$prediccionRL-resultados$test.AdoptionSpeed)
+resultados1 <- data.frame(test$AdoptionSpeed, prediccionRL1)
+resultados1$variacion <- abs(resultados1$prediccionRL1-resultados1$test.AdoptionSpeed)
 
-fitLMSpBPL<- lm(y~AdoptionSpeed, data = train)
-summary(fitLMSpBPL)
+fitLMSpBPL1<- lm(y~AdoptionSpeed, data = train)
+summary(fitLMSpBPL1)
 
-prediccionMSpByPL <- predict(fitLMSpBPL, newdata = test)
-resultados1 <- data.frame(test$y, round(prediccionMSpByPL, 0))
-names(resultados1) <- c("real", "prediccion")
-resultados1$real
-resultados1$prediccion
-confusionMatrix(table(resultados1$real, resultados1$prediccion))
+prediccionMSpByPL1 <- predict(fitLMSpBPL1, newdata = test)
+resultados11 <- data.frame(test$y, round(prediccionMSpByPL1, 0))
+names(resultados11) <- c("real", "prediccion")
+resultados11$real
+resultados11$prediccion
+confusionMatrix(table(resultados11$real, resultados11$prediccion))
+
+plot(resultados11$real, resultados11$prediccion, col = "blue", main = "AdoptionSpeed and Age related to Type", xlab = "Real data", ylab = "Predicted Data")
+abline(lm(resultados11$real ~ resultados11$prediccion))
+
+# Age y Breed1
+fitLMPW2 <- lm(Breed1~Age, data = train)
+prediccionRL2 <- predict(fitLMPW2, newdata = test)
+
+resultados2 <- data.frame(test$Breed1, prediccionRL2)
+resultados2$variacion <- abs(resultados2$test.Breed1-resultados2$prediccionRL2)
+
+fitLMSpBPL2<- lm(y~Breed1, data = train)
+summary(fitLMSpBPL2)
+
+prediccionMSpByPL2 <- predict(fitLMSpBPL2, newdata = test)
+resultados12 <- data.frame(test$y, round(prediccionMSpByPL2, 0))
+names(resultados12) <- c("real", "prediccion")
+resultados12$real
+resultados12$prediccion
+u2 <- union(resultados12$real, resultados12$prediccion)
+t2 <- table(factor(resultados12$real, u2), factor(resultados12$prediccion, u2))
+confusionMatrix(t2)
+
+plot(resultados12$real, resultados12$prediccion, col = "blue", main = "Breed and Vaccinated related to AdoptionSpeed", xlab = "Real data", ylab = "Predicted Data")
+abline(lm(resultados12$real ~ resultados12$prediccion))
+
+# Vaccinated y Age
+fitLMPW3 <- lm(Age~Vaccinated, data = train)
+prediccionRL3 <- predict(fitLMPW3, newdata = test)
+
+resultados3 <- data.frame(test$Age, prediccionRL3)
+resultados3$variacion <- abs(resultados$Age-resultados$prediccionRL3)
+
+fitLMSpBPL3<- lm(y~Age, data = train)
+summary(fitLMSpBPL3)
+
+prediccionMSpByPL3 <- predict(fitLMSpBPL3, newdata = test)
+resultados13 <- data.frame(test$y, round(prediccionMSpByPL3, 0))
+names(resultados13) <- c("real", "prediccion")
+resultados13$real
+resultados13$prediccion
+u3 <- union(resultados13$real, resultados13$prediccion)
+t3 <- table(factor(resultados13$real, u3), factor(resultados13$prediccion, u3))
+confusionMatrix(t3)
+
+plot(resultados13$real, resultados13$prediccion, col = "blue", main = "Breed and Vaccinated related to AdoptionSpeed", xlab = "Real data", ylab = "Predicted Data")
+abline(lm(resultados13$real ~ resultados13$prediccion))
+
+# Breed1 y Vaccinated
+fitLMPW4 <- lm(Vaccinated~Breed1, data = train)
+prediccionRL4 <- predict(fitLMPW4, newdata = test)
+
+resultados4 <- data.frame(test$Vaccinated, prediccionRL4)
+resultados4$variacion <- abs(resultados$Vaccinated-resultados$prediccionRL4)
+
+fitLMSpBPL4<- lm(y~Vaccinated, data = train)
+summary(fitLMSpBPL4)
+
+prediccionMSpByPL4 <- predict(fitLMSpBPL4, newdata = test)
+resultados14 <- data.frame(test$y, round(prediccionMSpByPL4, 0))
+names(resultados14) <- c("real", "prediccion")
+resultados14$real
+resultados14$prediccion
+u4 <- union(resultados14$real, resultados14$prediccion)
+t4 <- table(factor(resultados14$real, u4), factor(resultados14$prediccion, u4))
+confusionMatrix(t4)
+
+plot(resultados14$real, resultados14$prediccion, col = "blue", main = "Breed and Vaccinated related to AdoptionSpeed", xlab = "Real data", ylab = "Predicted Data")
+abline(lm(resultados14$real ~ resultados14$prediccion), col = "red")
+
+# Type y Age
+fitLMPW5 <- lm(Type~Age, data = train)
+prediccionRL5 <- predict(fitLMPW5, newdata = test)
+
+resultados5 <- data.frame(test$Type, prediccionRL5)
+resultados5$variacion <- abs(resultados5$prediccionRL5-resultados5$test.Type)
+
+fitLMSpBPL5<- lm(y~Type, data = train)
+summary(fitLMSpBPL5)
+
+prediccionMSpByPL5 <- predict(fitLMSpBPL5, newdata = test)
+resultados15 <- data.frame(test$y, round(prediccionMSpByPL5, 0))
+names(resultados15) <- c("real", "prediccion")
+resultados15$real
+resultados15$prediccion
+u5 <- union(resultados15$real, resultados15$prediccion)
+t5 <- table(factor(resultados15$real, u5), factor(resultados15$prediccion, u5))
+confusionMatrix(t5)
+
+plot(resultados15$real, resultados15$prediccion, col = "blue", main = "AdoptionSpeed and Age related to Type", xlab = "Real data", ylab = "Predicted Data")
+abline(lm(resultados15$real ~ resultados15$prediccion))
 
 # Regresión lineal multiple
 # --------------------------
